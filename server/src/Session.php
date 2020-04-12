388c2050e06ce710235ee67f91c7503d10d6dd0d
Eli Silakov <me@ileamare.ru>
2020-04-12 19:59:50 +0300

    add sessions dummies

---
+++ server/src/Session.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Leamare\ENyan;
+
+class Sessions {
+  // construct
+  // connect
+  // disconnect
+  // close
+  // get owner
+  // get next
+  // get prev
+}
---
+++ server/src/Sessions.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Leamare\ENyan;
+
+class Sessions {
+  // new session
+
+  // find session by conn
+
+  // get session
+}
