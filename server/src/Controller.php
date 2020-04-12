69ff9411c3e282d6c1265009145d9d20a4b14571
Eli Silakov <me@ileamare.ru>
2020-04-12 20:56:32 +0300

    support for all messages (controller)

--- server/src/Controller.php
+++ server/src/Controller.php
@@ -19,37 +19,41 @@ class Controller implements MessageComponentInterface {
   public function onMessage(ConnectionInterface $from, $msg) {
     // echo("[M] {$this->createConnIdString($conn)} says `$msg`\n");
 
-    $m = NyanCodes::decodeMsg($msg);
+    try {
+      $m = NyanCodes::decodeMsg($msg);
 
-    switch ($m['msg']) {
-      case NyanCodes::MsgCodes['NewSession']:
-        $this->newSession($from);
-        break;
-      case NyanCodes::MsgCodes['JoinSession']:
-        $this->joinSession($from, $m['params']);
-        break;
-      case NyanCodes::MsgCodes['EndSession']:
-        $this->endSession($from);
-        break;
-      case NyanCodes::MsgCodes['Disconnect']:
-        $this->disconnect($from);
-        break;
-      case NyanCodes::MsgCodes['SessionSettings']:
-        $this->updateSettings($from, $m['params']);
-        break;
-      case NyanCodes::MsgCodes['ApproachingEdge']:
-      case NyanCodes::MsgCodes['HitEdge']:
-      case NyanCodes::MsgCodes['Hidden']:
-        $this->reportStatus($from);
-        break;
-      default:
-        $this->sendError($from);
+      switch ($m['msg']) {
+        case NyanCodes::MsgCodes['NewSession']:
+          $this->newSession($from);
+          break;
+        case NyanCodes::MsgCodes['JoinSession']:
+          $this->joinSession($from, $m['params']);
+          break;
+        case NyanCodes::MsgCodes['EndSession']:
+          $this->endSession($from);
+          break;
+        case NyanCodes::MsgCodes['Disconnect']:
+          $this->disconnect($from);
+          break;
+        case NyanCodes::MsgCodes['SessionSettings']:
+          $this->updateSettings($from, $m['params']);
+          break;
+        case NyanCodes::MsgCodes['ApproachingEdge']:
+        case NyanCodes::MsgCodes['HitEdge']:
+        case NyanCodes::MsgCodes['Hidden']:
+          $this->reportStatus($from, $m['msg']);
+          break;
+        default:
+          $this->sendError($from);
+      }
+    } catch (\Exception $e) {
+      echo("[E] {$this->createConnIdString($from)} - can't process $msg - {$e->getMessage()}\n");
     }
   }
 
   public function onClose(ConnectionInterface $conn) {
     $this->disconnect($conn);
-    echo("[Z] Closed onnection - {$this->createConnIdString($conn)}\n");
+    echo("[Z] Closed connection - {$this->createConnIdString($conn)}\n");
   }
 
   public function onError(ConnectionInterface $conn, \Exception $e) {
@@ -68,15 +72,83 @@ class Controller implements MessageComponentInterface {
     echo("[M] Sent `$m` to {$this->createConnIdString($src)}\n");
   }
 
-  private function newSession(ConnectionInterface &$src) {}
+  private function newSession(ConnectionInterface &$src) {
+    $ssid = $this->sessions->new();
+    echo("[ ] Started session $ssid\n");
 
-  private function joinSession(ConnectionInterface &$src, array $params) {}
+    $this->sessions->get($ssid)->connect($src);
+    $m = NyanCodes::encodeMsg([
+      'event' => 'SessionStarted',
+      'params' => [ $ssid ]
+    ]);
+    $src->send($m);
+    //echo("[M] Sent `$m` to {$this->createConnIdString($src)}\n");
+  }
 
-  private function endSession(ConnectionInterface &$src) {}
+  private function joinSession(ConnectionInterface &$src, array $params) {
+    $ssid = $params[0];
+    if (!$this->sessions->is($ssid)) {
+      $this->sendError($src);
+      return;
+    }
+    $this->sessions->get($ssid)->connect($src);
+  }
 
-  private function disconnect(ConnectionInterface &$src) {}
+  private function endSession(ConnectionInterface &$src) {
+    $ssid = $this->sessions->findSessionByConn($src);
+    if (!$ssid) return;
+    $owner = $this->sessions->get($ssid)->getOwner();
+    if ($this->createConnIdString($owner) !== $this->createConnIdString($src)) {
+      $this->sendError($src);
+    }
+    $this->sessions->close($ssid);
+  }
 
-  private function reportStatus(ConnectionInterface &$src) {}
+  private function disconnect(ConnectionInterface &$src) {
+    $ssid = $this->sessions->findSessionByConn($src);
+    if (!$ssid) return;
+    $this->sessions->get($ssid)->disconnect($src);
+  }
 
-  private function updateSettings(ConnectionInterface &$src, array $params) {}
+  private function reportStatus(ConnectionInterface &$src, string $type) {
+    $ssid = $this->sessions->findSessionByConn($src);
+    if (!$ssid) return;
+    $s = $this->sessions->get($ssid);
+    if (!$s->isActive($src)) return;
+
+    switch ($type) {
+      case NyanCodes::MsgCodes['ApproachingEdge']:
+        $next = $s->getNext($src);
+        $m = NyanCodes::encodeMsg([
+          'event' => 'Prepare'
+        ]);
+        $next->send($m);
+        break;
+      case NyanCodes::MsgCodes['HitEdge']:
+        $next = $s->getNext($src);
+        $m = NyanCodes::encodeMsg([
+          'event' => 'StartMove'
+        ]);
+        $next->send($m);
+        $s->active($next);
+        break;
+    }
+  }
+
+  private function updateSettings(ConnectionInterface &$src, array $params) {
+    $ssid = $this->sessions->findSessionByConn($src);
+    if (!$ssid) return;
+    $owner = $this->sessions->get($ssid)->getOwner();
+    if ($this->createConnIdString($owner) !== $this->createConnIdString($src)) {
+      $this->sendError($src);
+    }
+
+    $s = $this->sessions->get($ssid);
+    $s->updateParams($params);
+    $m = NyanCodes::encodeMsg([
+      'event' => 'SettingsUpdate',
+      'params' => $params
+    ]);
+    $s->notifyAll($m);
+  }
 }
