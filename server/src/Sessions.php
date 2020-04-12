009dd5394cde78bfb7b398a259732692a3d34d84
Eli Silakov <me@ileamare.ru>
2020-04-12 20:57:40 +0300

    Sessions manager class

--- server/src/Sessions.php
+++ server/src/Sessions.php
@@ -2,10 +2,41 @@
 
 namespace Leamare\ENyan;
 
+use Ratchet\ConnectionInterface;
+
 class Sessions {
-  // new session
+  private $sessions = [];
 
-  // find session by conn
+  public function new() {
+    $nm = $this->generateSessionName();
+    $this->sessions[$nm] = new Session();
+    return $nm;
+  }
 
-  // get session
+  public function get(string $name) {
+    if (isset($this->sessions[$name]))
+      return $this->sessions[$name];
+    throw new \Exception("No session $name");
+  }
+
+  public function is(string $name) {
+    return isset($this->sessions[$name]);
+  }
+
+  public function close(string $name) {
+    if (isset($this->sessions[$name]))
+      unset($this->sessions[$name]);
+  }
+
+  public function findSessionByConn(ConnectionInterface $conn) {
+    foreach ($this->sessions as $id => $s) {
+      if ($s->isClient($conn))
+        return $id;
+    }
+    return null;
+  }
+
+  private function generateSessionName() {
+    return bin2hex(random_bytes(5));
+  }
 }
