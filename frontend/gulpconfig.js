22499c1e10bc9308853926e129f3a851a8fb79d6
Eli Silakov <me@ileamare.ru>
2020-04-17 00:21:16 +0300

    add gulp config and  other build related stuff

---
+++ frontend/gulpconfig.js
@@ -0,0 +1,169 @@
+'use strict';
+
+let config  = require('./config.json');
+
+const __BUILDID__ = (+new Date).toString(36);
+
+var src   = './src/',
+    build = './build/';
+
+var modRewrite  = require('connect-modrewrite');
+
+var getEnvPostfix = () => global.production ? "min." : "";
+
+var vendorList = [
+];
+
+
+module.exports = {
+  clean: {
+    src: [build+'**/*', '!'+build+'app/**']
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  styles: {
+    src: [
+      src + 'css/*.css',
+      src + 'assets/**/*.css',
+    ],
+    dest: build,
+    name: 'bundle.min.css',
+    cssnano: {
+      zindex: false
+    }
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  createVendorList: () => vendorList.map((currVal, index, array) => {
+    return currVal.replace('%PF.', getEnvPostfix());
+  }),
+
+  vendor: {
+    src: [],
+    dest: build,
+    name: 'vendor.js'
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  scripts: {
+    src: [
+      src + 'js/**/**/*.js',
+      src + 'js/**/*.js',
+      src + 'js/*.js',
+    ],
+    dest: build,
+    name: 'bundle.js'
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  templates: {
+    src: [
+      src + 'templ/*.html'
+    ],
+    dest: build + 'templates/',
+
+    // HTMLMinifier
+    // See: https://github.com/kangax/html-minifier
+    htmlmin: {
+      collapseWhitespace: true
+    }
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  images: [
+    {
+      src: [
+        src + 'assets/**/*.{png,jpg,svg}',
+      ],
+      dest: build + 'assets/'
+    }
+  ],
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  copy: [
+    // root dir
+    {
+      src: [
+        src + '*.*',
+        src + '.*',
+      ],
+      dest: build
+    },
+    {
+      src: [
+        src + 'assets/**/*.{gif,mp3}',
+      ],
+      binary: true,
+      dest: build + 'assets/'
+    },
+  ],
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  revision: {
+    assets: {
+      src: [
+        build + '*.min.css',
+        build + '*.min.js'
+      ],
+      base: build,
+      dest: build,
+      manifest: {path: 'rev-manifest.json'}
+    },
+    rename: {
+      src: build + '/*.html',
+      dest: build,
+      manifestSrc: build + 'rev-manifest.json',
+    }
+  },
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  watch: {
+    // BrowserSync. See more: https://www.browsersync.io/docs/options/
+    browserSync: {
+      // notify: true, // browser notification box (default: true)
+      open: false, // open browser after start (default: true)
+      // proxy: 'domain.ru' // wrap for real website (default: undefined)
+      server: {
+        baseDir: build,
+        // middleware: [
+        //     modRewrite([
+        //         '!\\.\\w+$ /index.html [L]'
+        //     ])
+        // ]
+      },
+    },
+
+    // Watch this directories
+    src: {
+      styles: [
+        src + 'css/*.css',
+        src + 'assets/**/*.css',
+      ],
+      scripts: [
+        src + 'js/**/**/*.js',
+        src + 'js/**/*.js',
+        src + 'js/*.js',
+      ],
+      templates: [
+        src + 'templ/*.html',
+      ],
+      images: src + [
+        src + 'res/**/*.{png,jpg,svg}',
+      ],
+      copy: [
+        src + 'index.html',
+        src + '*.*',
+        src + '.*',
+        src + 'assets/**/*.{gif,mp3}',
+      ],
+    }
+  }
+};
