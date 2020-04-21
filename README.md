673d9f8d2f6426e8f797824fd2316dcf29bc0a08
Eli Silakov <me@ileamare.ru>
2020-04-21 04:40:02 +0300

    readme

--- README.md
+++ README.md
@@ -4,6 +4,22 @@ ReactPHP + JS based endless Nyan Cat animation that can be run across multiple d
 
 https://lab.ileamare.ru/endl-nyan/
 
+## What is this?
+
+This is nyan cat that runs from one screen to another.
+
+Create session with "create/join" button to create a session. Type session ID and use the same button to join session.
+
+Click "move" to start moving!
+
+## Technical stuff
+
+Howler to have almost seamless audio.
+
+ReactPHP for server. Launch it with `php bin/server.php` in `server/` folder.
+
+You can run it in a simple socket mode for debug (see config examples).
+
 ## Why?
 
 because.
--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -7,6 +7,9 @@ let nyan = new Nyanner(
 document.getElementsByClassName('action-connector')[0].onclick = () => srv.ns();
 document.getElementsByClassName('action-disconnect')[0].onclick = () => srv.leave();
 document.getElementsByClassName('action-pause')[0].onclick = () => srv.pause();
+document.getElementsByClassName('action-question')[0].onclick = () => {
+  alert('This is nyan cat that runs from one screen to another. Create a session, give your code to friends so they join you. Start moving! https://github.com/leamare/endless-nyan')
+};
 document.getElementsByClassName('slider')[0].onchange = (e) => {
   let vol = +e.target.value/100;
   nyan.setVol(vol);
